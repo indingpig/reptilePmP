97b9d7f25448562efb9bb6cc8a4b3fcd5e5a8bf4
Sheldon <indoing@gmail.com>
2020-05-16 23:48:33 +0800

    更改cookie设置方式

--- login.js
+++ login.js
@@ -1,23 +1,46 @@
 const axios = require('axios');
 const superagent = require('superagent');
 const tools = require('./tools');
+const cheerio = require('cheerio');
+const config = require('./config');
 const analysisCsrfToken = tools.analysisCsrfToken;
-const getCookie = tools.getCookie;
 
 const browserMsg = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
   "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
 };
 
+const globalCookie = {};
+
+const getCookie = (res) => {
+  let cookieList = res.header['set-cookie'];
+  let targetCookieList = cookieList.map(ele => {
+    let strList = ele.split(';');
+    strList.forEach(item => {
+      let keyValue = item.split('=');
+      globalCookie[keyValue[0]] = keyValue[1];
+    });
+    return strList[0];
+  });
+  // console.log(globalCookie);
+  let cookie = ''
+  for (let x in globalCookie) {
+    cookie += `${x}=${globalCookie[x]}; `;
+  }
+  return cookie;
+};
+
+
 let csrfToken = '';
 let url = 'http://www.codingke.com/login';
-superagent.get(url).set(browserMsg).end((err, res) => {
-  if (err) throw err;
-  
-  let cookie = getCookie(res);
-  csrfToken = analysisCsrfToken(res);
-  loginFn(csrfToken, cookie);
-});
+const reptileFn = (url, browserMsg) => {
+  superagent.get(url).set(browserMsg).end((err, res) => {
+    if (err) throw err;
+    let cookie = getCookie(res);
+    csrfToken = analysisCsrfToken(res);
+    loginFn(csrfToken, cookie);
+  });
+}
 
 
 const loginFn = (CsrfToken, cookie) => {
@@ -28,9 +51,9 @@ const loginFn = (CsrfToken, cookie) => {
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7,zh-TW;q=0.6',
     Cookie: cookie,
     DNT: 1,
-    Host: 'www.codingke.com',
-    Origin: 'http://www.codingke.com',
-    Referer: "http://www.codingke.com/login",
+    // Host: 'www.codingke.com',
+    // Origin: 'http://www.codingke.com',
+    // Referer: "http://www.codingke.com/login",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
     "X-CSRF-Token": CsrfToken,
     // Connection: 'keep-alive',
@@ -46,9 +69,10 @@ const loginFn = (CsrfToken, cookie) => {
     .post('http://www.codingke.com/login_check')
     .send(loginData)
     .set(headers)
-    .then((data) => {
-      console.log(1);
-      headers.Cookie = getCookie(data);
+    .then((res) => {
+      // console.log(1);
+      headers.Cookie = getCookie(res);
+      // console.log(headers.Cookie);
       lesson(headers);
     })
     .catch(err => {
@@ -61,12 +85,33 @@ const lesson = (headers) => {
   superagent
     .get('http://www.codingke.com/study/v/11096-lesson')
     .set(headers)
-    .then((data) => {
+    .then((res) => {
       console.log('lesson');
-      console.log(data);
-      headers.Cookie = getCookie(data);
+      let $ = cheerio.load(res.text);
+      let courseId = $('#courseId').val();
+      let lessonId = $('#lessonId').val();
+      csrfToken = analysisCsrfToken(res);
+      let mediaUrl = `/study/${courseId}/lesson/${lessonId}?token=${csrfToken}c`;
+      getMediaHLSUrl(headers, mediaUrl, headers.Cookie);
     })
     .catch(err => {
       console.log(err);
     })
 }
+
+const getMediaHLSUrl = (headers, url) => {
+  superagent
+    .get(config.baseUrl + url)
+    .set(headers)
+    .then((res) => {
+      console.log('getMediaHLSUrl');
+      // let $ = cheerio.load(res.text);
+      console.log(res);
+      csrfToken = analysisCsrfToken(res);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
+module.exports = reptileFn;

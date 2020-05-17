0c6ee6430796f706937f78416767be42778b01b8
Sheldon <indoing@gmail.com>
2020-05-17 12:50:34 +0800

    分析m3u8文件

--- tools.js
+++ tools.js
@@ -1,4 +1,6 @@
 const cheerio = require('cheerio');
+const fs = require('fs');
+const path = require('path');
 const getCookie = (res) => {
   let cookieList = res.header['set-cookie'];
   let targetCookieList = cookieList.map(ele => {
@@ -18,7 +20,43 @@ const analysisCsrfToken = (res) => {
   return csrfToken;
 }
 
+const anlysisM3u8 = (filePath, dir) => {
+  let fileUrlList = [];
+  fs.readFile(filePath, (err, data) => {
+    let i = 0;
+    let content = data.toString();
+    let strList = content.split('\n');
+    let targetList = [];
+    let output = 'ffconcat version 1.0\n';
+    strList.forEach(item => {
+      if (item.includes('e=')) {
+        targetList.push(item);
+        fileUrlList.push('http://videocdn.codingke.com' + item);
+        let str = `${i}.ts`;
+        if (str.length < 9) {
+          output += `file ${completion(str)}\n`;
+          i++;
+        }
+      }
+    });
+    fs.writeFile(path.resolve(dir, 'input.txt'), output, err => {
+      if (err) throw err;
+    })
+  });
+}
+
+const completion = (str) => {
+  if (str.length < 9) {
+    let length = 9 - str.length ;
+    for (let i = 0; i < length; i++) {
+      str = '0' + str;
+    }
+  }
+  return str;
+}
+
 module.exports = {
   getCookie,
-  analysisCsrfToken
+  analysisCsrfToken,
+  anlysisM3u8
 }
